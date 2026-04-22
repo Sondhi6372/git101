f93edf329f49cb9917af24bcb226d139c082b0be
Sondhi6372 <sondhi.i@kkumail.com>
2026-04-22 14:20:23 +0100

    new forloops

--- too.java
+++ too.java
@@ -4,12 +4,13 @@ public class too {
         int a = 10;
         int b = 20;
         int sum = a + b;
+        int Sumloop = 0 ;
         System.out.println("The sum of a and b is: " + sum);
 
         int mul = a * b;
         System.out.println("The mul of a and b is: " + mul);
         for (int i = 0 ; i <= b ; i++ ){
-            sum += i;
+            Sumloop += i;
          
         }
     }
